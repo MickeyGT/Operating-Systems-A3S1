944b73ea89d3fab3f061d5f714806bf7a101bc31
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-10-25 23:43:00 +0300

    Update Homework2.c

--- Homework2.c
+++ Homework2.c
@@ -1,4 +1,3 @@
-    I    array.c                                 Row 1    Col 18   5:35  Ctrl-K H for help
 #include<stdlib.h>
 #include<stdio.h>
 #include<unistd.h>
@@ -6,41 +5,40 @@
 #include<sys/wait.h>
 #define WRITE 1
 #define READ 0
-
+int i;
+pid_t pid;
 int main()
 {
-        int arr[10]={2,3,4,2,5,2,1,4,3,2};
-        int searchN  = 2;
-        int NrProces = 5;
-        int pipefd[2];
-        int i;
-        pipe(pipefd);
-        pid_t ID;
-        for(i = 1; i <= NrProces; i++)
+    int v[12]={1,2,3,4,1,1,1,5,6,1,1,7};
+    int nrProces = 5;
+    int myPipe[2];
+    pipe(myPipe);
+    for(i = 0; i < nrProces; i++)
+    {
+        pid=fork();
+        if(pid == 0)
         {
-                ID=fork();
-                if(ID == 0)
-                {
-                        int j;
-                        for(j = (i-1)*(10/NrProces); j <= i*(10 / NrProces)-1; j++)
-                        {
-                                if(arr[j] == searchN)
-                                        write(pipefd[WRITE], &j, sizeof(j));
-                        }
-                        exit(0);
-                }
+            //Child process.
+            int j;
+            for(j = i*3; j <= 11&&j<=i*3+2; j++)
+            {
+                if(v[j] == 1)
+                    write(myPipe[WRITE], &j, sizeof(j));
+            }
+            exit(0);
         }
-        close(pipefd[WRITE]);
-        int poz;
-        printf("Numarul %d a fost gasit la poziti▒ile:\n ", searchN);
-        while(read(pipefd[READ], &poz, sizeof(poz)))
-        {
-                printf("%d, ", poz);
-        }
-        printf("\n");
-        for(i = 1; i <= NrProces; i++)
-        {
-                wait(0);
-        }
-        return 0;
+    }
+    close(myPipe[WRITE]);
+    int poz;
+    printf("Am gasit pe 1 la pozitiile:\n ");
+    while(read(myPipe[READ], &poz, sizeof(poz)))
+    {
+        printf("%d, ", poz);
+    }
+    printf("\n");
+    for(i = 1; i <= nrProces; i++)
+    {
+        wait(0);
+    }
+    return 0;
 }
